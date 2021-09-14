b3da02eac4eb08c1ad917af1beb884b0bfb52a3b
CarlosSantiago Nevett <csnevett@gmail.com>
2021-09-14 13:39:44 -0700

    reducers up and combined

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"><h1>test</h1></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { Provider } from 'react-redux';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import reducer from './reducers';
+import rootReducer from './reducers';
 
-const middleware = {
-  thunk,
-  logger,
-};
+const middleware = [thunk];
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+  middleware.push(logger);
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 ReactDOM.render(
   <React.StrictMode>
