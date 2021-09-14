a075c5d7e8f66155b3763b861f6744b6a47fee8e
CarlosSantiago Nevett <csnevett@gmail.com>
2021-09-14 13:27:16 -0700

    setting up store and router provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+import reducer from './reducers';
+
+const middleware = {
+  thunk,
+  logger,
+};
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
